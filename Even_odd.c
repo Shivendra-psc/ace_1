5b257f0ecd3ec640d5e5be6cbff4bd17223bb05d
Shivendra <shivendra.psc@gmail.com>
2017-08-19 11:55:44 +0530

    first commit

---
+++ Even_odd.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//16115075_clab_exp1(12-08-17)
+int main(){
+int num;
+printf("Enter a Number of your choice");
+scanf("%d",&num);
+if (num%2==0){
+printf("The number is even");
+}
+else{
+printf("the number is odd");
+}
+return 0;
+}
